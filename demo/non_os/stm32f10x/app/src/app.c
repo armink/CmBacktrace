057f6a30dec5ed20be259b9fb1845924416bffbc
armink <armink.ztl@gmail.com>
2016-12-27 15:28:51 +0800

    1、【增加】STM32F10X 裸机 Demo ，并提供 Keil 及 IAR 工程。
    Signed-off-by: armink <armink.ztl@gmail.com>

---
+++ demo/non_os/stm32f10x/app/src/app.c
@@ -0,0 +1,31 @@
+#include <bsp.h>
+#include <cm_backtrace.h>
+#include <stdio.h>
+
+#define HARDWARE_VERSION               "V1.0.0"
+#define SOFTWARE_VERSION               "V0.1.0"
+
+extern void fault_test_by_unalign(void);
+extern void fault_test_by_div0(void);
+
+int main(void){
+    uint8_t _continue;
+        
+    BSP_Init();
+    
+    /* CmBacktrace initialize */
+    cm_backtrace_init("CmBacktrace", HARDWARE_VERSION, SOFTWARE_VERSION);
+    
+//    fault_test_by_unalign();
+    fault_test_by_div0();
+    
+    _continue = 0;
+    while(_continue == 0) {
+        LED_RUN_ON;
+        delay(1000000);
+        LED_RUN_OFF;
+        delay(1000000);      
+    }
+    
+    return 0;
+}
---
+++ demo/non_os/stm32f10x/app/src/fault_test.c
@@ -0,0 +1,40 @@
+/*
+ * fault_test.c
+ *
+ *  Created on: 2016/12/25
+ *      Author: Armink
+ */
+
+#include <stdio.h>
+
+void fault_test_by_unalign(void) {
+    volatile int * SCB_CCR = (volatile int *) 0xE000ED14; // SCB->CCR
+    volatile int * p;
+    volatile int value;
+
+    *SCB_CCR |= (1 << 3); /* bit3: UNALIGN_TRP. */
+
+    p = (int *) 0x00;
+    value = *p;
+    printf("addr:0x%02X value:0x%08X\r\n", (int) p, value);
+
+    p = (int *) 0x04;
+    value = *p;
+    printf("addr:0x%02X value:0x%08X\r\n", (int) p, value);
+
+    p = (int *) 0x03;
+    value = *p;
+    printf("addr:0x%02X value:0x%08X\r\n", (int) p, value);
+}
+
+void fault_test_by_div0(void) {
+    volatile int * SCB_CCR = (volatile int *) 0xE000ED14; // SCB->CCR
+    int x, y, z;
+
+    *SCB_CCR |= (1 << 4); /* bit4: DIV_0_TRP. */
+
+    x = 10;
+    y = 0;
+    z = x / y;
+    printf("z:%d\n", z);
+}
